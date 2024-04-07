de23e6bdc6327b9aa185e2ec300c84dad8104a35
minkyeom <minkyeom@naver.com>
2024-04-07 16:18:48 +0900

    project_progress and summary

---
+++ Django_hobby/프로젝트 진행상황 요약 및 정리.py
@@ -0,0 +1,29 @@
+# 개발 과정 요약 및 진행상황 정리 
+
+# vir_hoddy: book mark project_start
+# 리눅스 환경과 mac os, window 상황 고려
+
+# 가상공간 활성화(in pycharm)
+# pycharm_activate: vir_test/Scripts/activate
+
+#04.07
+"""
+프로젝트 파일 및 설정
+-allowed_host 설정
+-애플리케이션 등록
+-템플릿 설정
+-데이터베이스 엔진 설정
+-타임존 지정
+-미디어 설정
+
+기본 테이블 생성
+
+슈퍼 유저 생성
+
+애플리케이션 생성
+
+애플리케이션 등록
+"""
+
+
+
