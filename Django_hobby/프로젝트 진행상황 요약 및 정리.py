11ac23db92cbdfa8e754b86931ab31a80552b267
minkyeom <minkyeom@naver.com>
2024-04-08 17:36:08 +0900

    admin 사이트에 테이블 반영 및 데이터베이스 변경에 따른 오류 발생 및 시도 방향 정리

--- Django_hobby/프로젝트 진행상황 요약 및 정리.py
+++ Django_hobby/프로젝트 진행상황 요약 및 정리.py
@@ -1,10 +1,17 @@
-# 개발 과정 요약 및 진행상황 정리 
+# 개발 과정 요약 및 진행상황 정리
 
 # vir_hoddy: book mark project_start
 # 리눅스 환경과 mac os, window 상황 고려
 
 # 가상공간 활성화(in pycharm)
 # pycharm_activate: vir_test/Scripts/activate
+
+#04.06
+"""
+git허브 내 보안 문제 오류 등을 상세히 검토 후 올리는 여부 검토!! 필요성 인지
+
+"""
+
 
 #04.07
 """
@@ -26,4 +33,24 @@
 """
 
 
+#4.08
 
+"""
+오늘 한 작업 내용 및 오류 발생 
+admin 사이트에 테이블 반영 관련 시도 및 오류 발생 
+데이터베이스 변경 사항 반영 
+
+#bookmark pip 오류 발생 가상 환경 내 설치 오류 발생 및 해결 과정 점검
+#pip install 오류 발생 
+
+환경 변수 설정 및 고려 
+
+#해결 방향 및 시도 방향 
+pip 업그레이드
+-오류 발생 및 리눅스 윈도우 터미널마다 명령어의 방향성이 조금씩 다름 확인
+python -m pip install --upgrade pip 실행
+
+파이썬 버전 다운그레이드 실행 및 점검 확인
+
+
+"""
